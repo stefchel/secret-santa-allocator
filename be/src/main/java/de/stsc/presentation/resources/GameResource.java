a017fad02a3e9fee6e5b00d977171fdc2806ee67
Stefan Schiechel <stefanschiechel@gmx.de>
2023-06-05 20:34:46 +0200

    moved participant endpoints to game resource

--- be/src/main/java/de/stsc/presentation/resources/GameResource.java
+++ be/src/main/java/de/stsc/presentation/resources/GameResource.java
@@ -3,15 +3,15 @@ package de.stsc.presentation.resources;
 import de.stsc.persistance.entity.Game;
 import de.stsc.persistance.entity.Participant;
 import de.stsc.presentation.dto.GameDto;
+import de.stsc.presentation.dto.ParticipantDto;
 import de.stsc.presentation.mapper.GameMapper;
+import de.stsc.presentation.mapper.ParticipantMapper;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -21,42 +21,47 @@ import java.util.stream.Collectors;
 public class GameResource {
 
     @Inject
-    private GameMapper mapper;
+    private GameMapper gameMapper;
+    @Inject
+    private ParticipantMapper participantMapper;
 
     @GET
     public List<GameDto> getAll() {
         return Game.<Game>listAll().stream()
-                .map(mapper::toDto)
+                .map(gameMapper::toDto)
                 .collect(Collectors.toList());
     }
 
     @GET
     @Path("/{gid}")
     public GameDto getById(long gid) {
-        return mapper.toDto(Game.findById(gid));
+        return gameMapper.toDto(Game.findById(gid));
     }
 
     @POST
     @Transactional
     public GameDto newGame(GameDto dto) {
-        Game game = mapper.fromDto(dto);
+        Game game = gameMapper.fromDto(dto);
         game.persist();
-        return mapper.toDto(game);
+        return gameMapper.toDto(game);
     }
 
-    @PUT
-    @Path("/{gid}/participant/{pid}")
+    @POST
+    @Path("/{gid}/participant")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
-    public GameDto addParticipant(long gid, long pid) {
-        Optional<Participant> p = Participant.findByIdOptional(pid);
+    public GameDto addParticipant(long gid, ParticipantDto participantDto) {
         Optional<Game> g = Game.findByIdOptional(gid);
-        if (p.isEmpty() || g.isEmpty()) {
+        if (g.isEmpty()) {
             throw new IllegalArgumentException("id is null");
-        } else {
-            g.get().addParticipant(p.get());
-            g.get().persist();
-            return mapper.toDto(g.get());
         }
+
+        // ToDo unique name
+        Participant participant = participantMapper.fromDto(participantDto);
+        participant.persist();
+        g.get().addParticipant(participant);
+        g.get().persist();
+        return gameMapper.toDto(g.get());
     }
 
     @GET
